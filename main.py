75526dbe59b4b4db7617f391b9915e74a1a6698f
barisumut <barisumutbaykal@gmail.com>
2023-05-07 18:25:41 +0300

    Add description on the new columns

--- main.py
+++ main.py
@@ -12,3 +12,9 @@ with col2:
     Hi, I am Barış! I am a python enthusiast, mechanical engineer and full-time learner.
     """
     st.info(content)
+
+content2= """
+Below you can find some of the apps I have built in Python. Feel free to contact me! 
+"""
+
+st.write(content2)
